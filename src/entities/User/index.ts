0c600894f5101847c7097b0d2a7a2d3d8342cd12
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-07 13:51:28 -0300

    feat: crate entitie `User`

---
+++ src/entities/User/index.ts
@@ -0,0 +1,32 @@
+const UserPropertiesValues = {
+  PHONE_LENGTH: 11,
+  LENGTH_FOR_NAME: 2,
+} as const;
+
+function phoneValidator(phone: string) {
+  return (
+    User.removePhoneStr(phone).length === UserPropertiesValues.PHONE_LENGTH
+  );
+}
+
+function removePhoneStr(phone: string) {
+  return phone.replace(/[^0-9]/g, "");
+}
+
+function phoneMask(phone: string) {
+  const numeros = removePhoneStr(phone);
+
+  const regex = /^(\d{2})(\d{1})(\d{4})(\d{4})$/;
+
+  const resultado = numeros.replace(regex, "($1) $2 $3-$4");
+
+  return resultado;
+}
+
+const User = {
+  removePhoneStr,
+  phoneValidator,
+  phoneMask,
+};
+
+export default User;
