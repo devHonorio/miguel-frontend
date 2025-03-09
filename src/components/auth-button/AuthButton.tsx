887236b5bf1b116f272d99541e10801ec4593c15
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-09 16:29:57 -0300

    feat: create component `<AuthButton/>`

---
+++ src/components/auth-button/AuthButton.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { deleteCookie, getCookie } from "cookies-next";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
+export const AuthButton = () => {
+  const pathName = usePathname();
+
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    setLogin(!!getCookie("token"));
+  }, []);
+
+  if (pathName === "/login") {
+    return;
+  }
+
+  return (
+    <Link href="/login">
+      {login && (
+        <Button
+          size="sm"
+          variant="destructive"
+          onClick={() => {
+            deleteCookie("token");
+          }}
+        >
+          Deslogar
+        </Button>
+      )}
+
+      {!login && (
+        <Button size="sm" variant="secondary">
+          Logar
+        </Button>
+      )}
+    </Link>
+  );
+};
