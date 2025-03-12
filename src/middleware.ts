bd5bd4e3d822ddfa50b2417c5373e97059e8e852
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-12 00:06:11 -0300

    feat: config middleware for admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,53 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
+interface PathType {
+  [key: string]: (req: NextRequest, token?: string) => unknown;
+}
+
+const secret = new TextEncoder().encode(process.env.SECRET);
+
+const verifyIsAdmin = async (token: string) => {
+  const { payload } = await jwtVerify(token, secret);
+
+  return payload.is_admin as boolean;
+};
+
+const paths: PathType = {
+  "/admin/cups": async (req, token) => {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    const isAdmin = await verifyIsAdmin(token);
+
+    if (!isAdmin) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  "/login": async (req, token) => {
+    if (token) {
+      const isAdmin = await verifyIsAdmin(token);
+
+      if (isAdmin)
+        return NextResponse.redirect(new URL("/admin/cups", req.url));
+
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+};
 export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+
+  if (!paths[pathname]) return;
+
   const cookiesStore = await cookies();
 
   const token = cookiesStore.get("token")?.value;
 
-  if (token) {
-    const secret = new TextEncoder().encode(process.env.SECRET);
-
-    const { payload } = await jwtVerify(token, secret);
-
-    if (payload.is_admin)
-      return NextResponse.redirect(new URL("/admin/cups", req.url));
-
-    return NextResponse.redirect(new URL("/catalogo", req.url));
-  }
+  return paths[pathname](req, token);
 }
 
 export const config = {
-  matcher: "/login",
+  matcher: ["/login", "/admin/:path*"],
 };
