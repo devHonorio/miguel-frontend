927824e1892463e673bc9353b3e9bcc9fba6c0f3
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-12 21:28:03 -0300

    feat: change color background

--- src/app/(public)/catalogo/layout.tsx
+++ src/app/(public)/catalogo/layout.tsx
@@ -2,7 +2,7 @@ import { LayoutProps } from "@/app/layout";
 
 export default function CatalogLayout({ children }: LayoutProps) {
   return (
-    <div className="bg-secondary/5 flex flex-1 flex-wrap content-center justify-center gap-10 p-10">
+    <div className="bg-primary/5 flex flex-1 flex-wrap content-center justify-center gap-10 p-10">
       {children}
     </div>
   );
