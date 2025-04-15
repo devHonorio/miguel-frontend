b609d9da960cd8554ae0e5510ba4c83eb01a8766
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-04-14 22:51:11 -0300

    feat: create store for order

---
+++ src/app/store/order.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { CupUpdateType } from "../(private)/admin/cups/schema";
+import { AdditionalType } from "../(private)/admin/additional/hooks/schema";
+
+interface OrderStore {
+  cups: CupStore[];
+  addCup: (cup: CupStore) => void;
+  removeCup: (id: string) => void;
+}
+
+interface CupStore extends Pick<CupUpdateType, "size" | "price" | "id"> {
+  additional: Pick<AdditionalType, "id" | "name">[];
+}
+
+export const useOrderStore = create<OrderStore>((set) => ({
+  cups: [],
+  addCup: (cup: CupStore) => set((state) => ({ cups: [...state.cups, cup] })),
+  removeCup(id) {
+    return set((state) => ({
+      cups: state.cups.filter((cup) => cup.id !== id),
+    }));
+  },
+}));
