9f97002ae183259f888a89580bb2bd2ac4caa848
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-04-14 23:04:25 -0300

    feat: use storage

--- src/app/(public)/cup/[id]/components/card-cup/CardCup.tsx
+++ src/app/(public)/cup/[id]/components/card-cup/CardCup.tsx
@@ -2,6 +2,7 @@
 
 import { AdditionalType } from "@/app/(private)/admin/additional/hooks/schema";
 import { CupUpdateType } from "@/app/(private)/admin/cups/schema";
+import { useOrderStore } from "@/app/store/order";
 import { toBRL } from "@/app/utils";
 import {
   CardCupContainer,
@@ -12,6 +13,7 @@ import {
 } from "@/components/card-cup";
 import { Button } from "@/components/ui/button";
 import { Check, Plus } from "lucide-react";
+import Link from "next/link";
 import { parseAsArrayOf, parseAsString, useQueryState } from "nuqs";
 
 interface CardCupProps
@@ -45,6 +47,7 @@ export const CardCup = ({
   const priceCup =
     price + additionalListSelected.reduce((acc, { price }) => acc + price, 0);
 
+  const { addCup } = useOrderStore();
   return (
     <>
       <CardCupContainer>
@@ -97,8 +100,55 @@ export const CardCup = ({
       </CardCupContainer>
 
       <div className="flex gap-5 rounded-4xl bg-white p-10">
-        <Button variant="outline">Finalizar pedido</Button>
-        <Button variant="secondary">Adicionar mais um</Button>
+        <Link href="/orderDetails">
+          <Button
+            variant="outline"
+            onClick={() => {
+              addCup({
+                additional: additionalListId.map((id) => {
+                  const currentAdditional = additional.find(
+                    (add) => add.id === id,
+                  );
+
+                  return {
+                    name: currentAdditional!.name,
+                    id: currentAdditional?.id,
+                  };
+                }),
+                id: crypto.randomUUID(),
+                price: priceCup,
+                size,
+              });
+            }}
+          >
+            Finalizar pedido
+          </Button>
+        </Link>
+
+        <Link href="/catalogo">
+          <Button
+            variant="secondary"
+            onClick={() => {
+              addCup({
+                additional: additionalListId.map((id) => {
+                  const currentAdditional = additional.find(
+                    (add) => add.id === id,
+                  );
+
+                  return {
+                    name: currentAdditional!.name,
+                    id: currentAdditional?.id,
+                  };
+                }),
+                id: crypto.randomUUID(),
+                price: priceCup,
+                size,
+              });
+            }}
+          >
+            Adicionar mais um
+          </Button>
+        </Link>
       </div>
     </>
   );
