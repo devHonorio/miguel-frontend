55dc2e4cabaf3502069cc925608ccdd69b9d9e49
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-04-14 23:24:07 -0300

    feat: add persist localStorage

--- src/app/store/order.ts
+++ src/app/store/order.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { CupUpdateType } from "../(private)/admin/cups/schema";
 import { AdditionalType } from "../(private)/admin/additional/hooks/schema";
 
@@ -12,12 +13,18 @@ interface CupStore extends Pick<CupUpdateType, "size" | "price" | "id"> {
   additional: Pick<AdditionalType, "id" | "name">[];
 }
 
-export const useOrderStore = create<OrderStore>((set) => ({
-  cups: [],
-  addCup: (cup: CupStore) => set((state) => ({ cups: [...state.cups, cup] })),
-  removeCup(id) {
-    return set((state) => ({
-      cups: state.cups.filter((cup) => cup.id !== id),
-    }));
-  },
-}));
+export const useOrderStore = create<OrderStore>()(
+  persist(
+    (set) => ({
+      cups: [],
+      addCup: (cup: CupStore) =>
+        set((state) => ({ cups: [...state.cups, cup] })),
+      removeCup(id) {
+        return set((state) => ({
+          cups: state.cups.filter((cup) => cup.id !== id),
+        }));
+      },
+    }),
+    { name: "order" },
+  ),
+);
